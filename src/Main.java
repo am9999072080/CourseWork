795c23f766ff0b72037c2f3e62c74480f8a0b840
am999 <am9999072080@gmail.com>
2023-04-06 11:48:08 +0300

    Commit 1 Базовая сложность

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,75 @@
 import course.Employee;
 public class Main {
     public static void main(String[] args) {
+        Employee employee1 = new Employee("Иванов", "Иван", "Иванович", 1, 2_000);
+        Employee employee2 = new Employee("Сергеев", "Сергей", "Сергеевич", 2, 2_200);
+        Employee employee3 = new Employee("Алексеев", "Алексей", "Алексеевич", 3, 2_500);
+        Employee employee4 = new Employee("Александров", "Александр", "Александрович", 4, 2_300);
+        Employee employee5 = new Employee("Иванов", "Иван", "Иванович", 5, 2_400);
 
+        Employee[] vaults = new Employee[5];
+        vaults[0] = employee1;
+        vaults[1] = employee2;
+        vaults[2] = employee3;
+        vaults[3] = employee4;
+        vaults[4] = employee5;
+
+        for (Employee vault : vaults) {
+            System.out.println(vault);
+        }
+        System.out.println();
+        System.out.println("Сумма затрат в месяц: " + sum(vaults) + "$");
+        System.out.println();
+        System.out.println("Средняя зарплата в месяц: " + aver(vaults) + "$");
+        System.out.println();
+        System.out.println("Сотрудника с минимальной зарплатой: " + min(vaults) + "$");
+        System.out.println();
+        System.out.println("Сотрудника с максимальной зарплатой: " + max(vaults) + "$");
+        System.out.println();
+        printFio(vaults);
+    }
+
+    public static int sum(Employee[] vaults) {
+        int totalAmount = 0;
+        for (Employee vault : vaults) {
+            totalAmount += vault.getSalary();
+        }
+        return totalAmount;
+    }
+
+    public static double aver(Employee[] vaults) {
+        double sum = 0;
+        double average = 0;
+        for (Employee vault : vaults) {//использовал цикл for each.
+            sum += vault.getSalary();
+        }
+        average = sum / vaults.length;
+        return average;
+    }
+
+    public static Employee max(Employee[] vaults) {
+        Employee maximumSpend = vaults[0];
+        for (Employee vault : vaults) {
+            if (vault.getSalary() > maximumSpend.getSalary()) {
+                maximumSpend = vault;
+            }
+        }
+        return maximumSpend;
+    }
+
+    public static Employee min(Employee[] vaults) {
+        Employee minimumSpend = max(vaults);
+        for (Employee vault : vaults) {
+            if (vault.getSalary() < minimumSpend.getSalary()) {
+                minimumSpend = vault;
+            }
+        }
+        return minimumSpend;
+    }
+
+    public static void printFio(Employee[] vaults) {
+        for (Employee vault : vaults) {
+            System.out.println(vault.getName() + " " + vault.getMiddleName() + " " + vault.getSurname());
+        }
     }
 }
--- src/course/Employee.java
+++ src/course/Employee.java
@@ -1,4 +1,84 @@
 package course;
 
 public class Employee {
+    private String name;
+    private String surname;
+    private String middleName;
+
+    private int department;
+    private int salary;
+
+    private int id;
+    static int counter = 1;
+
+
+    public Employee(String surname, String name, String middleName, int department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getMiddleName() {
+
+        return middleName;
+    }
+
+    public int getDepartment() {
+
+        return department;
+    }
+
+    public int getSalary() {
+
+        return salary;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+
+        this.surname = surname;
+    }
+
+    public void setMiddleName(String middleName) {
+
+        this.middleName = middleName;
+    }
+
+    public void setDepartment(int department) {
+
+        this.department = department;
+    }
+
+    public void setMiddleSalary(int salary) {
+        this.salary = salary;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return id + "․ "
+                + surname + " "
+                + name + " "
+                + middleName + ", Отдел №-"
+                + department + ", "
+                + salary + "$";
+    }
 }
